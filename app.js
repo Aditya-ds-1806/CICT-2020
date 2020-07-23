c9f65eb4c928568d7ac250a70aebcfd4d2e28d13
Aditya D.S <adithya18062000@gmail.com>
2020-07-23 22:06:38 +0530

    new route and corresponding ejs template for committees

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-// const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/committees', (req, res) => {
+    res.render('committees');
 });
 
 app.get('*', (req, res) => {
