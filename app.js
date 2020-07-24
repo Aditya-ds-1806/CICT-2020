62cc756d739eb1d06848c676156ac3910bc2d33d
Aditya D.S <adithya18062000@gmail.com>
2020-07-24 19:38:54 +0530

    committtees page ready

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { route: "index" });
 });
 
 app.get('/committees', (req, res) => {
-    res.render('committees');
+    res.render('committees', { route: "committees" });
 });
 
 app.get('*', (req, res) => {
