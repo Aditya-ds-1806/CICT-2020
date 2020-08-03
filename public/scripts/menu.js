e7edb0c7c49f17ca6f18119831930f88095010e4
Aditya D.S <adithya18062000@gmail.com>
2020-08-03 23:12:44 +0530

    UI improvements

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -5,7 +5,7 @@ var programs = $('#programs');
 var close = $('#close');
 var stickyNav = $("nav.fixed-top");
 
-window.onload = function () {
+window.addEventListener('load', function () {
     authors.removeClass('d-none');
     programs.removeClass('d-none');
     authors.slideUp();
@@ -42,16 +42,19 @@ window.onload = function () {
         closeItem(authors, authorsBtn);
         programsBtn.toggleClass('text-white text-bahama-blue bg-white');
     });
-}
+});
 
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', toggleStickyNav);
+window.addEventListener('load', toggleStickyNav);
+
+function toggleStickyNav() {
     if (window.scrollY > window.innerHeight) {
         if (stickyNav.hasClass('d-none')) stickyNav.removeClass('d-none');
         stickyNav.fadeIn();
     } else {
         stickyNav.fadeOut();
     }
-})
+}
 
 function closeItem(item, btn) {
     if (isOpen(item)) {
